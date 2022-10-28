29f9eef79e75ed5bb7b2611165bc0a86e6992310
AndOliver46 <anderson_souza75@live.com>
2022-10-28 19:41:00 -0300

    remocao de comentario

--- funcionarios.h
+++ funcionarios.h
@@ -5,8 +5,6 @@
 #include "bibliotecas.h"
 #include "departamentos.h"
 
-
-//comentario teste
 typedef struct
 {
     char flogin[50], fNome[50], fSenha[30], fAtivoInativo[8],fpermi[10], fCpf[12], fCep[12], fTelefone[12], fCargo[30], fDataCadastro[11];
